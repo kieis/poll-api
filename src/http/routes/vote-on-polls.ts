ce5c92b9944bbb898b8fda37b9b203351c05522c
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-02-08 02:01:57 -0300

    feat: added vote on poll route

---
+++ src/http/routes/vote-on-polls.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { randomUUID } from "crypto";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+    const { pollId } = voteOnPollParams.parse(request.params);
+
+    const { sessionId } = request.cookies;
+    if (!sessionId) {
+      const generatedSessionId = randomUUID();
+
+      reply.cookie("sessionId", generatedSessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30,
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    return reply.status(201).send({});
+  });
+}
