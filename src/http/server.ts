5aa145acb736291ee428bbe55dfec47e22d07e5e
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-02-08 02:02:20 -0300

    build: added fastify cookie dependency

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,13 +1,21 @@
 import fastify from "fastify";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-polls";
+import fastifyCookie from "@fastify/cookie";
 
 const app = fastify();
 const port: number = process.env.PORT ? Number(process.env.PORT) : 3333;
 
-app.get("/", (req, res) => {
-  return res.status(200).send({
-    success: true,
-  });
+app.register(fastifyCookie, {
+  secret: process.env.COOKIE_SECRET,
+  hook: "onRequest",
+  parseOptions: {},
 });
+
+app.register(getPoll);
+app.register(createPoll);
+app.register(voteOnPoll);
 
 app.listen({ port }).then(() => {
   console.log(`HTTP Server listening on port ${port}`);
